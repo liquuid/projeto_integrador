a9f2819e2098e3f9535bf61acc2593630d5fd1ee
Fernando Henrique <liquuid@gmail.com>
2018-10-17 17:16:19 -0300

    conexão com wifi

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <ESP8266WiFi.h>
+#define SSID_REDE     "liquuidN"  // nome da rede
+#define SENHA_REDE    "3g3g3g3g"  //senha da rede
+
+WiFiClient client;
+ 
+void FazConexaoWiFi(void);
+float FazLeituraUmidade(void);
+ 
+/*
+ * Implementações
+ */
+ 
+ 
+//Função: faz a conexão WiFI
+//Parâmetros: nenhum
+//Retorno: nenhum
+void FazConexaoWiFi(void)
+{
+    client.stop();
+    Serial.println("Conectando-se à rede WiFi...");
+    Serial.println();  
+    delay(1000);
+    WiFi.begin(SSID_REDE, SENHA_REDE);
+ 
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connectado com sucesso!");  
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+ 
+    delay(1000);
+}
+ 
+//Função: faz a leitura do nível de umidade
+//Parâmetros: nenhum
+//Retorno: umidade percentual (0-100)
+//Observação: o ADC do NodeMCU permite até, no máximo, 3.3V. Dessa forma,
+//            para 3.3V, obtem-se (empiricamente) 978 como leitura de ADC
+float FazLeituraUmidade(void)
+{
+    int ValorADC;
+    float UmidadePercentual;
+ 
+     ValorADC = analogRead(0);   //978 -> 3,3V
+     Serial.print("[Leitura ADC] ");
+     Serial.println(ValorADC);
+      
+     UmidadePercentual = 100 * ((978-(float)ValorADC) / 978);
+     Serial.print("[Umidade Percentual] ");
+     Serial.print(UmidadePercentual);
+     Serial.println("%");
+ 
+     return UmidadePercentual;
+}
+void setup()
+{  
+    Serial.begin(9600);
+    FazConexaoWiFi();
+    Serial.println("Planta IoT com ESP8266 NodeMCU");
+}
+ 
+//loop principal
+void loop()
+{
+    float UmidadePercentualLida;
+    int UmidadePercentualTruncada;
+    char FieldUmidade[11];
+     
+    //Força desconexão ao ThingSpeak (se ainda estiver desconectado)
+    if (client.connected())
+    {
+        client.stop();
+        Serial.println("- Desconectado do ThingSpeak");
+        Serial.println();
+    }
+ 
+    UmidadePercentualLida = FazLeituraUmidade();
+    UmidadePercentualTruncada = (int)UmidadePercentualLida; //trunca umidade como número inteiro
+    delay(1000);
+}
